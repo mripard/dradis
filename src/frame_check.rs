b87ce3d76b151e26cecce99c00313a6a20be3fa5
Maxime Ripard <maxime@cerno.tech>
2022-12-15 16:09:27 +0100

    dradis: frame_check: Review panic conditions and improve errors
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- src/frame_check.rs
+++ src/frame_check.rs
@@ -4,17 +4,22 @@ use image::{DynamicImage, GenericImage, GenericImageView, ImageBuffer, Rgb, Rgba
 use log::{debug, warn};
 use rqrr::PreparedImage;
 use serde::Deserialize;
-use strum_macros::Display;
+use thiserror::Error;
 use twox_hash::XxHash64;
 
 const HEADER_VERSION_MAJOR: u8 = 2;
 
-#[derive(Display, Debug)]
+#[derive(Debug, Error)]
 enum FrameError {
-    Invalid,
-}
+    #[error("Frame Integrity Check Failed.")]
+    IntegrityFailure,
 
-impl std::error::Error for FrameError {}
+    #[error("Frame Header is Invalid.")]
+    InvalidFrame,
+
+    #[error("Not Enough Memory.")]
+    NotEnoughMemory,
+}
 
 #[allow(dead_code)]
 #[derive(Deserialize)]
@@ -32,14 +37,14 @@ pub fn decode_and_check_frame(
     data: &[u8],
     args: Option<(Option<usize>, usize, usize)>,
 ) -> std::result::Result<usize, Box<dyn std::error::Error>> {
-    let args = args.unwrap();
+    let args = args.expect("Missing arguments");
     let last_frame_index = args.0;
-    let width = args.1;
-    let height = args.2;
+    let width = u32::try_from(args.1).expect("Width doesn't fit into a u32");
+    let height = u32::try_from(args.2).expect("Height doesn't fit into a u32");
 
     let pixels = data.to_vec();
-    let buffer =
-        ImageBuffer::<Rgb<u8>, Vec<u8>>::from_vec(width as u32, height as u32, pixels).unwrap();
+    let buffer = ImageBuffer::<Rgb<u8>, Vec<u8>>::from_vec(width, height, pixels)
+        .ok_or(FrameError::NotEnoughMemory)?;
 
     let mut image = DynamicImage::ImageRgb8(buffer);
     let luma = image.to_luma8().view(0, 0, 128, 128).to_image();
@@ -48,17 +53,18 @@ pub fn decode_and_check_frame(
     let grids = prepared.detect_grids();
     if grids.len() != 1 {
         warn!("Didn't find a QR Code");
-        return Err(FrameError::Invalid)?;
+        return Err(FrameError::InvalidFrame)?;
     }
 
     let grid = &grids[0];
-    let (_, content) = grid.decode().unwrap();
+    let (_, content) = grid.decode().map_err(|_| FrameError::InvalidFrame)?;
 
-    let metadata: Metadata = serde_json::from_str(&content).unwrap();
+    let metadata: Metadata =
+        serde_json::from_str(&content).map_err(|_| FrameError::IntegrityFailure)?;
 
     if metadata.version.0 != HEADER_VERSION_MAJOR {
         warn!("Metadata Version Mismatch");
-        return Err(FrameError::Invalid)?;
+        return Err(FrameError::IntegrityFailure)?;
     }
 
     if let Some(last_index) = last_frame_index {
@@ -66,7 +72,7 @@ pub fn decode_and_check_frame(
 
         if index < last_index {
             warn!("Frame Index Mismatch");
-            return Err(FrameError::Invalid)?;
+            return Err(FrameError::IntegrityFailure)?;
         } else if index == last_index {
             debug!("Source cannot keep up?");
         } else if index > last_index + 1 {
@@ -74,9 +80,14 @@ pub fn decode_and_check_frame(
         }
     }
 
-    for x in 0..metadata.qrcode_width {
-        for y in 0..metadata.qrcode_height {
-            image.put_pixel(x as u32, y as u32, Rgba([0, 0, 0, 255]));
+    let qrcode_width =
+        u32::try_from(metadata.qrcode_width).expect("QR Code Width doesn't fit into a u32");
+    let qrcode_height =
+        u32::try_from(metadata.qrcode_height).expect("QR Code Height doesn't fit into a u32");
+
+    for x in 0..qrcode_width {
+        for y in 0..qrcode_height {
+            image.put_pixel(x, y, Rgba([0, 0, 0, 255]));
         }
     }
 
@@ -89,7 +100,7 @@ pub fn decode_and_check_frame(
             "Hash mismatch: {:#x} vs expected {:#x}",
             hash, metadata.hash
         );
-        return Err(FrameError::Invalid)?;
+        return Err(FrameError::IntegrityFailure)?;
     }
 
     Ok(metadata.index)
