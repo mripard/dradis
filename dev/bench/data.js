dd496317ccfee667fa6b1b94ebf079df47e90749
github-action-benchmark <github@users.noreply.github.com>
2025-03-28 13:34:46 +0000

    add Dradis Benchmark (cargo) benchmark result for d9903faa4086fa2e6cb10b717262fd10829c8db2

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1743168886661,
+  "repoUrl": "https://github.com/mripard/dradis",
+  "entries": {
+    "Dradis Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "mripard@kernel.org",
+            "name": "Maxime Ripard",
+            "username": "mripard"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d9903faa4086fa2e6cb10b717262fd10829c8db2",
+          "message": "Merge pull request #201 from mripard/tracing-improvements\n\nAdd more traces and create some benchmarks, with CI",
+          "timestamp": "2025-03-28T14:30:07+01:00",
+          "tree_id": "dfcc635e6f84ac02f5b4b4a290c43312c6bef357",
+          "url": "https://github.com/mripard/dradis/commit/d9903faa4086fa2e6cb10b717262fd10829c8db2"
+        },
+        "date": 1743168885972,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "frame processing/whole",
+            "value": 8459380,
+            "range": "± 9153",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
