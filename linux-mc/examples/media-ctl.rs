24663a674dc93a02ce71f7ab2050f24405fc1a0d
Maxime Ripard <mripard@kernel.org>
2025-04-11 13:02:03 +0200

    dradis: Add new media controller crate
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

---
+++ linux-mc/examples/media-ctl.rs
@@ -0,0 +1,167 @@
+#![allow(missing_docs)]
+
+use std::{io, path::PathBuf};
+
+use clap::Parser;
+use linux_mc::{
+    MediaController, MediaControllerPadKind, RevocableResult, RevocableValue, try_result, try_value,
+};
+
+fn dump_topology(media: &MediaController) -> RevocableResult<(), io::Error> {
+    let info = media.info().unwrap();
+    println!(
+        "Media controller API version {}",
+        info.media_controller_version()
+    );
+    println!();
+    println!("Media device information");
+    println!("------------------------");
+    println!("driver\t\t{}", info.driver());
+    println!("model\t\t{}", info.model());
+    println!("serial\t\t{}", info.serial());
+    println!("bus info\t{}", info.bus_info());
+    println!("hw revision\t{:#x}", info.hardware_revision());
+    println!("driver version\t{}", info.driver_version());
+    println!();
+    println!(
+        "Device topology version {}",
+        media.topology_version().unwrap()
+    );
+
+    let entities = media.entities().unwrap();
+    let links = media.links().unwrap();
+    let pads = media.pads().unwrap();
+
+    for entity in &entities {
+        let entity_interfaces = try_result!(entity.interfaces());
+
+        let entity_pads: Vec<_> = pads
+            .iter()
+            .filter(|p| p.entity_id() == entity.id())
+            .collect();
+
+        let mut args = Vec::new();
+        if !entity_pads.is_empty() {
+            args.push(if entity_pads.len() > 1 {
+                format!("{} pads", entity_pads.len())
+            } else {
+                format!("{} pad", entity_pads.len())
+            });
+        }
+
+        let outbound_links: Vec<_> = links
+            .iter()
+            .filter(|l| {
+                entity_pads.iter().any(|e| e.id() == l.source_id())
+                    || entity_pads.iter().any(|e| e.id() == l.sink_id())
+            })
+            .collect();
+        if !outbound_links.is_empty() {
+            args.push(if outbound_links.len() > 1 {
+                format!("{} links", outbound_links.len())
+            } else {
+                format!("{} link", outbound_links.len())
+            });
+        }
+
+        println!(
+            "- entity {}: {}{}",
+            try_value!(entity.id()),
+            try_value!(entity.name()),
+            if args.is_empty() {
+                String::new()
+            } else {
+                format!(" ({})", args.join(", "))
+            }
+        );
+
+        let flags = try_value!(entity.flag_names())
+            .collect::<Vec<&str>>()
+            .join(", ");
+
+        print!("            {}", try_value!(entity.function()));
+        if !flags.is_empty() {
+            print!(", Flags: {flags}");
+        }
+        println!();
+
+        if let Some(itf) = entity_interfaces.first() {
+            let device_node = try_result!(itf.device_node());
+
+            println!(
+                "            device node name {}",
+                device_node.unwrap().path().display()
+            );
+        }
+
+        for pad in entity_pads {
+            let pad_kind = try_value!(pad.kind());
+
+            let link = match pad_kind {
+                MediaControllerPadKind::Sink => {
+                    links.iter().find(|l| l.sink_id() == pad.id()).unwrap()
+                }
+                MediaControllerPadKind::Source => {
+                    links.iter().find(|l| l.source_id() == pad.id()).unwrap()
+                }
+            };
+
+            let remote_pad = try_result!(pad.remote_pad()).unwrap();
+            let remote_entity = try_result!(remote_pad.entity());
+
+            let flags = try_value!(pad.flag_names())
+                .collect::<Vec<&str>>()
+                .join(", ");
+            print!("	pad{}:", try_value!(pad.index()));
+            if !flags.is_empty() {
+                print!(" Flags: {flags}");
+            }
+            println!();
+
+            let flags = try_value!(link.flag_names())
+                .collect::<Vec<&str>>()
+                .join(", ");
+            println!(
+                "		{} \"{}\":{}, Type {}{}",
+                match pad_kind {
+                    MediaControllerPadKind::Sink => "<-",
+                    MediaControllerPadKind::Source => "->",
+                },
+                try_value!(remote_entity.name()),
+                try_value!(remote_pad.index()),
+                try_value!(link.kind()),
+                if flags.is_empty() {
+                    String::new()
+                } else {
+                    format!(", Flags: {flags}")
+                }
+            );
+        }
+
+        println!();
+    }
+
+    RevocableResult::Ok(())
+}
+
+#[derive(Parser)]
+struct CliArgs {
+    #[arg(short, long, default_value = "/dev/media0")]
+    device: PathBuf,
+}
+
+fn main() -> Result<(), io::Error> {
+    let args = CliArgs::parse();
+
+    let media = MediaController::new(&args.device).unwrap();
+
+    loop {
+        match dump_topology(&media) {
+            RevocableResult::Ok(()) => break,
+            RevocableResult::Revoked => {}
+            RevocableResult::Err(e) => return Err(e),
+        }
+    }
+
+    Ok(())
+}
---
+++ linux-raw/src/lib.rs
@@ -0,0 +1,369 @@
+#![doc = include_str!("../README.md")]
+
+use core::{cmp::Ordering, fmt, num::ParseIntError, str::FromStr};
+
+/// A kernel version representation
+#[derive(Clone, Debug)]
+pub struct KernelVersion {
+    major: u16,
+    minor: u8,
+    patch: u8,
+    sublevel: Option<u8>,
+    extraversion: Option<String>,
+}
+
+impl KernelVersion {
+    /// Creates a new kernel version, with no sublevel version number or extra version.
+    #[must_use]
+    pub fn new(major: u16, minor: u8, patch: u8) -> Self {
+        Self {
+            major,
+            minor,
+            patch,
+            sublevel: None,
+            extraversion: None,
+        }
+    }
+
+    /// Major Version Number
+    #[must_use]
+    pub fn major(&self) -> u16 {
+        self.major
+    }
+
+    /// Minor Version Number
+    #[must_use]
+    pub fn minor(&self) -> u8 {
+        self.minor
+    }
+
+    /// Patch Version Number
+    #[must_use]
+    pub fn patch(&self) -> u8 {
+        self.patch
+    }
+
+    /// Sublevel Version Number
+    ///
+    /// This was used on Linux up to (and including) Linux 2.6.39 stable releases.
+    #[must_use]
+    pub fn sublevel(&self) -> Option<u8> {
+        self.sublevel
+    }
+
+    /// Extra Version
+    #[must_use]
+    pub fn extraversion(&self) -> Option<&str> {
+        self.extraversion.as_deref()
+    }
+}
+
+impl fmt::Display for KernelVersion {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!("{}.{}.{}", self.major, self.minor, self.patch))
+    }
+}
+
+impl PartialOrd for KernelVersion {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self.major.partial_cmp(&other.major) {
+            Some(Ordering::Equal) => {}
+            ord => return ord,
+        }
+
+        match self.minor.partial_cmp(&other.minor) {
+            Some(Ordering::Equal) => {}
+            ord => return ord,
+        }
+
+        match self.patch.partial_cmp(&other.patch) {
+            Some(Ordering::Equal) => {}
+            ord => return ord,
+        }
+
+        match self.sublevel.partial_cmp(&other.sublevel) {
+            Some(Ordering::Equal) => {}
+            ord => return ord,
+        }
+
+        // We ignore extraversion when comparing
+        Some(Ordering::Equal)
+    }
+}
+
+impl PartialEq for KernelVersion {
+    fn eq(&self, other: &Self) -> bool {
+        PartialOrd::partial_cmp(&self, &other) == Some(Ordering::Equal)
+    }
+}
+
+impl From<u32> for KernelVersion {
+    fn from(value: u32) -> Self {
+        let major = ((value >> 16) & Into::<u32>::into(u16::MAX))
+            .try_into()
+            .expect("I'm terrible at math.");
+
+        let minor = ((value >> 8) & Into::<u32>::into(u8::MAX))
+            .try_into()
+            .expect("I'm terrible at math.");
+
+        let patch = (value & Into::<u32>::into(u8::MAX))
+            .try_into()
+            .expect("I'm terrible at math.");
+
+        Self {
+            major,
+            minor,
+            patch,
+            sublevel: None,
+            extraversion: None,
+        }
+    }
+}
+
+/// An error returned when parsing a kernel version
+#[derive(Debug, PartialEq)]
+pub enum KernelVersionParseError {
+    /// The format of the version String is invalid
+    InvalidFormat,
+
+    /// A version number can't be parsed
+    ParseInt(ParseIntError),
+}
+
+impl From<ParseIntError> for KernelVersionParseError {
+    fn from(err: ParseIntError) -> Self {
+        KernelVersionParseError::ParseInt(err)
+    }
+}
+
+impl FromStr for KernelVersion {
+    type Err = KernelVersionParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.splitn(2, '-').collect::<Vec<_>>();
+
+        let version_str = parts[0];
+        let extraversion = if parts.len() > 1 {
+            Some(parts[1].to_owned())
+        } else {
+            None
+        };
+
+        let mut version_items = version_str.split('.');
+
+        let major = version_items
+            .next()
+            .ok_or(KernelVersionParseError::InvalidFormat)?
+            .parse()?;
+
+        let minor = version_items
+            .next()
+            .ok_or(KernelVersionParseError::InvalidFormat)?
+            .parse()?;
+
+        let patch = version_items
+            .next()
+            .ok_or(KernelVersionParseError::InvalidFormat)?
+            .parse()?;
+
+        let sublevel = if let Some(item) = version_items.next() {
+            Some(item.parse()?)
+        } else {
+            None
+        };
+
+        // Fail if we have numbers after that
+        if version_items.next().is_some() {
+            return Err(KernelVersionParseError::InvalidFormat);
+        }
+
+        Ok(KernelVersion {
+            major,
+            minor,
+            patch,
+            sublevel,
+            extraversion,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use crate::KernelVersion;
+
+    #[test]
+    fn test_from_str() {
+        // Fedora Kernel
+        assert_eq!(
+            KernelVersion::from_str("6.14.6-300.fc42.x86_64").unwrap(),
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: None,
+                extraversion: Some(String::from("300.fc42.x86_64"))
+            }
+        );
+
+        // Ubuntu Kernel
+        assert_eq!(
+            KernelVersion::from_str("4.10.0-28-generic").unwrap(),
+            KernelVersion {
+                major: 4,
+                minor: 10,
+                patch: 0,
+                sublevel: None,
+                extraversion: Some(String::from("28-generic"))
+            }
+        );
+
+        // Random STB running 2.6.29
+        assert_eq!(
+            KernelVersion::from_str("2.6.29.6-22-sigma").unwrap(),
+            KernelVersion {
+                major: 2,
+                minor: 6,
+                patch: 29,
+                sublevel: Some(6),
+                extraversion: Some(String::from("22-sigma"))
+            }
+        );
+    }
+
+    #[test]
+    fn test_ord() {
+        assert_eq!(
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            },
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            },
+        );
+
+        assert!(
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            } > KernelVersion {
+                major: 5,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            },
+        );
+
+        assert!(
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            } > KernelVersion {
+                major: 6,
+                minor: 13,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            },
+        );
+
+        assert!(
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            } > KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 5,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            },
+        );
+
+        assert!(
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            } > KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(11),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            },
+        );
+
+        assert!(
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            } > KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: None,
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            },
+        );
+
+        assert_eq!(
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            },
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("200.fc42.x86_64")),
+            },
+        );
+
+        assert_eq!(
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: Some(String::from("300.fc42.x86_64")),
+            },
+            KernelVersion {
+                major: 6,
+                minor: 14,
+                patch: 6,
+                sublevel: Some(12),
+                extraversion: None,
+            },
+        );
+    }
+}
