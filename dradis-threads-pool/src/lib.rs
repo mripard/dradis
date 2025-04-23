220b7c358181d12d6fc61208392e247cdc3e9061
Maxime Ripard <mripard@kernel.org>
2025-04-23 09:19:13 +0200

    threads-pool: Enable lints
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- dradis-threads-pool/src/lib.rs
+++ dradis-threads-pool/src/lib.rs
@@ -1,10 +1,14 @@
-use std::{
-    collections::VecDeque,
-    thread::{self, JoinHandle},
-};
+//! Thread Spawner with optional limit Library
+
+extern crate alloc;
+
+use alloc::collections::VecDeque;
+use std::thread::{self, JoinHandle};
 
 use tracing::warn;
 
+/// A Thread dispatcher, with an optional limit on the number of threads that can be spawned.
+#[derive(Debug)]
 pub struct ThreadPool<T> {
     queue: VecDeque<JoinHandle<T>>,
     limit: Option<usize>,
@@ -14,6 +18,8 @@ impl<T> ThreadPool<T>
 where
     T: Send + 'static,
 {
+    /// Creates a new [`ThreadPool`] instance.
+    #[must_use]
     pub fn new(limit: Option<usize>) -> Self {
         Self {
             queue: VecDeque::new(),
@@ -21,6 +27,7 @@ where
         }
     }
 
+    /// Spawns a thread that will run the given closure, if we are not over the limit.
     pub fn spawn_and_queue<F>(&mut self, f: F)
     where
         F: FnOnce() -> T + Send + 'static,
