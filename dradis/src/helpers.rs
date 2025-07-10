mripard/dradis

commit 11431b75ff5cb43c8da89c1e0a9b48df33c5581f

build(deps): bump redid from `4e966cc` to `862de87`
Bumps [redid](https://github.com/mripard/redid) from `4e966cc` to `862de87`.
- [Commits](https://github.com/mripard/redid/compare/4e966cc0eddfa5ff4dab84bcac4db92ef18c097a...862de8718e007f8c6e84ba7505f490536ac365ef)

---
updated-dependencies:
- dependency-name: redid
  dependency-version: 862de8718e007f8c6e84ba7505f490536ac365ef
  dependency-type: direct:production
...

Signed-off-by: dependabot[bot] <support@github.com>

dradis/src/helpers.rs
@@ -12,6 +12,7 @@ use std::{
 use num_traits::{One, ToPrimitive, Zero};
 use redid::{
     EdidChromaticityPoint, EdidChromaticityPoints, EdidDescriptorDetailedTiming,
+    EdidDescriptorDetailedTimingHorizontal, EdidDescriptorDetailedTimingVertical,
     EdidDetailedTimingDigitalSeparateSync, EdidDetailedTimingDigitalSync,
     EdidDetailedTimingDigitalSyncKind, EdidDetailedTimingStereo, EdidDetailedTimingSync,
     EdidDisplayColorType, EdidDisplayTransferCharacteristics, EdidEstablishedTiming, EdidExtension,
@@ -247,7 +248,9 @@ pub(crate) fn bridge_set_edid(
                         .dfp1_compatible(true)
                         .build(),
                 ))
-                .display_transfer_characteristic(EdidDisplayTransferCharacteristics::try_from(2.2)?)
+                .display_transfer_characteristics(EdidDisplayTransferCharacteristics::try_from(
+                    2.2,
+                )?)
                 .feature_support(
                     EdidR3FeatureSupport::builder()
                         .display_type(EdidDisplayColorType::RGBColor)
@@ -270,21 +273,29 @@ pub(crate) fn bridge_set_edid(
                 .build(),
         ))
         .add_established_timing(EdidEstablishedTiming::ET_640_480_60hz)
-        .add_descriptor(EdidR3Descriptor::DetailedTiming(
+        .preferred_timing(
             EdidDescriptorDetailedTiming::builder()
                 .pixel_clock(dtd.clock_khz.try_into()?)
-                .horizontal_front_porch(dtd.hfp.try_into()?)
-                .horizontal_addressable(dtd.hdisplay.try_into()?)
-                .horizontal_blanking((dtd.hfp + dtd.hsync + dtd.hbp).try_into()?)
-                .horizontal_sync_pulse(dtd.hsync.try_into()?)
-                .horizontal_border(0.try_into()?)
-                .horizontal_size(1600.try_into()?)
-                .vertical_front_porch(dtd.vfp.try_into()?)
-                .vertical_addressable(dtd.vdisplay.try_into()?)
-                .vertical_blanking(u16::from(dtd.vfp + dtd.vsync + dtd.vbp).try_into()?)
-                .vertical_sync_pulse(dtd.vsync.try_into()?)
-                .vertical_border(0.try_into()?)
-                .vertical_size(900.try_into()?)
+                .horizontal(
+                    EdidDescriptorDetailedTimingHorizontal::builder()
+                        .active(dtd.hdisplay.try_into()?)
+                        .border(0.try_into()?)
+                        .front_porch(dtd.hfp.try_into()?)
+                        .sync_pulse(dtd.hsync.try_into()?)
+                        .back_porch(dtd.hbp.try_into()?)
+                        .size_mm(1600.try_into()?)
+                        .build(),
+                )
+                .vertical(
+                    EdidDescriptorDetailedTimingVertical::builder()
+                        .active(dtd.vdisplay.try_into()?)
+                        .border(0.try_into()?)
+                        .front_porch(dtd.vfp.try_into()?)
+                        .sync_pulse(dtd.vsync.try_into()?)
+                        .back_porch(dtd.vbp.try_into()?)
+                        .size_mm(900.try_into()?)
+                        .build(),
+                )
                 .sync_type(EdidDetailedTimingSync::Digital(
                     EdidDetailedTimingDigitalSync::builder()
                         .kind(EdidDetailedTimingDigitalSyncKind::Separate(
@@ -297,7 +308,7 @@ pub(crate) fn bridge_set_edid(
                 ))
                 .stereo(EdidDetailedTimingStereo::None)
                 .build(),
-        ))
+        )
         .add_descriptor(EdidR3Descriptor::ProductName("Dradis".try_into()?))
         .add_descriptor(EdidR3Descriptor::DisplayRangeLimits(
             EdidR3DisplayRangeLimits::builder()

dradis/src/main.rs
@@ -589,7 +589,7 @@ struct TestEdidDetailedTiming {
     hsync: u16,
     vfp: u8,
     vdisplay: u16,
-    vbp: u8,
+    vbp: u16,
     vsync: u8,
 }
 

commit 7944b06c59572ac1eacbb39a662facdf959b1b2d

dradis: Fix clippy error
`dtd.vbp` is a u16 now and clippy throws this error:

   error: useless conversion to the same type: `u16`
      --> dradis/src/helpers.rs:227:57
       |
   227 |  / u32::from(u16::from(dtd.vfp) + dtd.vdisplay
       |  + u16::from(dtd.vbp) + u16::from(dtd.vsync));
       |    ^^^^^^^^^^ help: consider removing `u16::from()`: `dtd.vbp`
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#useless_conversion

Fixes: 11431b75ff5c ("build(deps): bump redid from `4e966cc` to `862de87`")

Signed-off-by: José Expósito <jose.exposito89@gmail.com>

dradis/src/helpers.rs
@@ -224,7 +224,7 @@ pub(crate) fn bridge_set_edid(
     )))?;
 
     let mode_vfreq_hz = mode_hfreq_hz
-        / u32::from(u16::from(dtd.vfp) + dtd.vdisplay + u16::from(dtd.vbp) + u16::from(dtd.vsync));
+        / u32::from(u16::from(dtd.vfp) + dtd.vdisplay + dtd.vbp + u16::from(dtd.vsync));
     let min_vfreq_hz = round_down(min(mode_vfreq_hz, VIC_1_VFREQ_HZ), VFREQ_TOLERANCE_HZ)
         .to_u8()
         .ok_or(SetupError::Value(String::from(