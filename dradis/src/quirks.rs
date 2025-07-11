bbeccdc8bd623cdd9a9bcf3118e7467ad87fb0bb
José Expósito <jose.exposito89@gmail.com>
2025-07-11 17:03:10 +0200

    dradis: Add quirks for Raspberry Pi 5 + GeekWorm C779
    On Raspberry Pi 5 the C779 bridge requires this link:
    
      $ media-ctl -d /dev/mediaX \
        -l ''\''csi2'\'':4 -> '\''rp1-cfe-csi2_ch0'\'':0 [1]'
    
    For more information, see:
    https://wiki.geekworm.com/CSI_Manual_on_Pi_5
    
    Signed-off-by: José Expósito <jose.exposito89@gmail.com>

--- dradis/src/quirks.rs
+++ dradis/src/quirks.rs
@@ -11,7 +11,13 @@ type QuirkFunction = fn(&MediaController, &[PipelineItem]) -> Result<(), io::Err
 
 /// Applies, if required, quirks to a pair of device plus bridge.
 pub fn apply_quirks(mc: &MediaController, pipeline: &[PipelineItem]) -> Result<(), io::Error> {
-    let quirks: HashMap<&str, QuirkFunction> = HashMap::from([]);
+    let quirks: HashMap<&str, QuirkFunction> = HashMap::from([
+        // Raspberry Pi 5 + GeekWorm C779
+        (
+            "rp1-cfe-csi2_ch0:tc358743 11-000f",
+            rpi5_geekworm_c779 as QuirkFunction,
+        ),
+    ]);
 
     let PipelineItem(_, root, _) = pipeline
         .first()
@@ -32,4 +38,24 @@ pub fn apply_quirks(mc: &MediaController, pipeline: &[PipelineItem]) -> Result<(
     }
 
     Ok(())
+}
+
+/// On Raspberry Pi 5 the C779 bridge requires this link:
+/// $ media-ctl -d /dev/mediaX -l ''\''csi2'\'':4 -> '\''rp1-cfe-csi2_ch0'\'':0 [1]'
+/// For more information, see: <https://wiki.geekworm.com/CSI_Manual_on_Pi_5>
+fn rpi5_geekworm_c779(mc: &MediaController, pipeline: &[PipelineItem]) -> Result<(), io::Error> {
+    let PipelineItem(sink, _, _) = pipeline
+        .first()
+        .ok_or(io::Error::new(Errno::NODEV.kind(), "Missing Root Entity"))?;
+
+    let root_sink_pad = sink
+        .as_ref()
+        .ok_or(io::Error::new(Errno::NODEV.kind(), "Missing Root Sink Pad"))?;
+
+    let csi2_source_pad = root_sink_pad.remote_pad().valid()?.ok_or(io::Error::new(
+        Errno::NODEV.kind(),
+        "Missing CSI2 Source Pad",
+    ))?;
+
+    mc.setup_link(&csi2_source_pad, root_sink_pad, 1)
 }
