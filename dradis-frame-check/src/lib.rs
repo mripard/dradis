167c01f13da2eaecd028e97408151afacd1fa859
Maxime Ripard <mripard@kernel.org>
2025-09-25 13:49:15 +0200

    wip

--- dradis-frame-check/src/lib.rs
+++ dradis-frame-check/src/lib.rs
@@ -8,13 +8,15 @@ extern crate alloc;
 use alloc::{rc::Rc, sync::Arc};
 use core::{cell::RefCell, fmt, hash::Hasher as _, ops::Deref};
 use rxing::{
-    BarcodeFormat, BinaryBitmap, DecodeHints, Luma8LuminanceSource, MultiFormatReader, Reader as _,
-    common::HybridBinarizer,
+    BarcodeFormat, BinaryBitmap, DecodeHints, Luma8LuminanceSource, LuminanceSource,
+    MultiFormatReader, Reader as _, common::HybridBinarizer,
 };
 use std::{
+    borrow::Cow,
     collections::HashSet,
     fs::{self, File},
     io::{self, BufWriter},
+    ops::Range,
     path::Path,
 };
 
@@ -23,7 +25,7 @@ use pix::{
     bgr::{Bgr8, Bgra8},
     chan::Ch8,
     el::Pixel,
-    gray::Gray8,
+    gray::{Gray, Gray8},
     rgb::Rgb8,
 };
 use png::{BitDepth, ColorType, Encoder};
@@ -94,6 +96,70 @@ impl fmt::Display for Metadata {
             self.index,
             self.hash,
         ))
+    }
+}
+
+struct CustomRgb24Source {
+    luma: Box<[u8]>,
+    width: usize,
+    height: usize,
+}
+
+impl CustomRgb24Source {
+    fn new_with_region<P>(pixels: &FrameInner<P>, region: Region) -> Self
+    where
+        P: FramePixel + Pixel<Chan = Ch8>,
+    {
+        let mut luma =
+            Vec::<u8>::with_capacity((region.height() * region.width()).try_into().unwrap());
+
+        for row_idx in region.top()..region.bottom() {
+            for col_idx in region.left()..region.right() {
+                let pixel = pixels.pixel(col_idx as u32, row_idx as u32);
+                let gray = pixel.convert::<Gray8>();
+
+                luma.push(Gray::value(gray).into())
+            }
+        }
+
+        Self {
+            luma: luma.into_boxed_slice(),
+            height: region.height().try_into().unwrap(),
+            width: region.width().try_into().unwrap(),
+        }
+    }
+}
+
+impl LuminanceSource for CustomRgb24Source {
+    const SUPPORTS_CROP: bool = false;
+    const SUPPORTS_ROTATION: bool = false;
+
+    fn get_row(&self, y: usize) -> Option<Cow<'_, [u8]>> {
+        todo!()
+    }
+
+    fn get_column(&self, _x: usize) -> Vec<u8> {
+        unimplemented!()
+    }
+
+    fn get_matrix(&self) -> Vec<u8> {
+        self.luma.to_vec()
+    }
+
+    fn get_width(&self) -> usize {
+        self.width
+    }
+
+    fn get_height(&self) -> usize {
+        self.height
+    }
+
+    fn invert(&mut self) {
+        unimplemented!()
+    }
+
+    fn get_luma8_point(&self, x: usize, y: usize) -> u8 {
+        todo!()
     }
 }
 
@@ -255,18 +321,16 @@ where
     /// IF the QR Code can't be decoded
     pub fn qrcode_content(&self) -> Result<String, FrameError> {
         debug_span!("QR Code Detection").in_scope(|| {
-            let cropped = self.0.crop(QRCODE_WIDTH, QRCODE_HEIGHT);
-            let luma = cropped.to_luma();
-
             let mut reader = MultiFormatReader::default();
 
             let results = reader
                 .decode_with_hints(
-                    &mut BinaryBitmap::new(HybridBinarizer::new(Luma8LuminanceSource::new(
-                        luma.as_u8_slice().to_vec(),
-                        QRCODE_WIDTH,
-                        QRCODE_HEIGHT,
-                    ))),
+                    &mut BinaryBitmap::new(HybridBinarizer::new(
+                        CustomRgb24Source::new_with_region(
+                            &self.0,
+                            Region::new(0, 0, QRCODE_WIDTH, QRCODE_HEIGHT),
+                        ),
+                    )),
                     &DecodeHints {
                         PossibleFormats: Some(HashSet::from([BarcodeFormat::QR_CODE])),
                         TryHarder: Some(true),
