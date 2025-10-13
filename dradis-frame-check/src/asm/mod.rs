ca8393f85bf9623c74c2fe57f9a29eca54e636e4
Maxime Ripard <mripard@kernel.org>
2025-10-13 09:41:46 +0200

    dradis-frame-check: aarch64: Add neon optimized memcpy
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- dradis-frame-check/src/asm/mod.rs
+++ dradis-frame-check/src/asm/mod.rs
@@ -6,19 +6,32 @@ mod aarch64 {
     use std::arch::global_asm;
 
     global_asm!(include_str!("./aarch64/memcpy.S"));
+    global_asm!(include_str!("./aarch64/memcpy-advsimd.S"));
 
     extern "C" {
         pub(crate) fn __memcpy_aarch64(dst: *mut c_void, src: *const c_void, count: usize);
+        pub(crate) fn __memcpy_aarch64_simd(dst: *mut c_void, src: *const c_void, count: usize);
     }
 
     pub(crate) fn optimized_memcpy<T>(dst: *mut T, src: *const T, count: usize) {
-        unsafe {
-            __memcpy_aarch64(
-                dst.cast::<c_void>(),
-                src.cast::<c_void>(),
-                count * size_of::<T>(),
-            )
-        };
+        if cfg!(target_feature = "neon") {
+            unsafe {
+                __memcpy_aarch64_simd(
+                    dst.cast::<c_void>(),
+                    src.cast::<c_void>(),
+                    count * size_of::<T>(),
+                )
+            };
+        } else {
+            unreachable!();
+            unsafe {
+                __memcpy_aarch64(
+                    dst.cast::<c_void>(),
+                    src.cast::<c_void>(),
+                    count * size_of::<T>(),
+                )
+            };
+        }
     }
 }
 
